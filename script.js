0688a5e33a65361e7a5f881db78f117a53f00929
Jasmine Anderson <jazz2900@gmail.com>
2019-07-09 17:03:48 -0700

    completed page 2 and 3 of tutorial

--- script.js
+++ script.js
@@ -1,20 +1,31 @@
 var canvas = document.getElementById("myCanvas");
 var ctx = canvas.getContext("2d");
+var ballRadius = 10;
+var x = canvas.width/2;
+var y = canvas.height-30;
+var dx = 2;
+var dy = -2;
 
-ctx.beginPath();
-ctx.rect(20, 40, 50, 50);
-ctx.fillStyle = "#FF0000";
-ctx.fill();
-ctx.closePath();
+function drawBall() {
+    ctx.beginPath();
+    // x-axis, y-axies, radias, starting angle
+    ctx.arc(x, y, ballRadius, 0, Math.PI*2);
+    ctx.fillStyle = "#0095DD";
+    ctx.fill();
+    ctx.closePath();
+}
+function draw() {
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    drawBall();
+    if(x + dx > canvas.width || x + dx < 0) {
+        dx = -dx;
+    }
+    
+    if(y + dy > canvas.height || y +dy < 0) {
+        dy = -dy
+    }
+    x += dx;
+    y += dy;
+}
 
-ctx.beginPath();
-ctx.arc(240, 160, 20, 0, Math.PI*2, false);
-ctx.fillStyle = "green";
-ctx.fill();
-ctx.closePath();
-
-ctx.beginPath();
-ctx.rect(160, 10, 100, 40);
-ctx.strokeStyle = "rgba(0, 0, 255, 0.5)";
-ctx.stroke();
-ctx.closePath();
+setInterval(draw, 10);
