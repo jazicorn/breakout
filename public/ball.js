3b7b184ca53492aefdb281398e4beeebcc004337
Jasmine Anderson <jazz2900@gmail.com>
2019-07-16 13:43:49 -0700

    refactored ball class

---
+++ public/ball.js
@@ -0,0 +1,15 @@
+class Ball {
+    constructor(radius, color = "#289C99") {
+        this.radius = radius;
+        this.color = color;
+    }
+    render(ctx) {
+        ctx.beginPath();
+        ctx.arc(x, y, this.radius, 0, Math.PI *2);
+        ctx.fillStyle = this.color;
+        ctx.fill();
+        ctx.closePath();
+    }
+}
+
+const sball = new Ball(5);
--- public/main.js
+++ public/main.js
@@ -138,22 +138,6 @@ console.log(arr[0]) // 1
 const barr = [[1,2,3], [11,22,33], [333,111,222]]
 barr[0][1]
 
-class Ball {
-    constructor(radius, color = "#289C99") {
-        this.radius = radius;
-        this.color = color;
-    }
-    render(ctx) {
-        ctx.beginPath();
-        ctx.arc(x, y, this.radius, 0, Math.PI *2);
-        ctx.fillStyle = this.color;
-        ctx.fill();
-        ctx.closePath();
-    }
-}
-
-const sball = new Ball(5);
-
 // function drawBall() {
 //     // ctx.beginPath();
 //     // // x-axis, y-axies, radias, starting angle
